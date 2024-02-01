3350b153c263dd14b095a56a4bf718219d968d92
Alex Kotlar <akotlar@bu.edu>
2024-02-01 03:07:20 +0000

    Issue #382 #383: Remove Attrs from Ancestry and Convert to Genotype Dosage Matrix

--- python/python/bystro/ancestry/tests/test_ancestry_types.py
+++ python/python/bystro/ancestry/tests/test_ancestry_types.py
@@ -1,16 +1,13 @@
 """Test ancestry_types.py."""
+import re
 
-from attrs.exceptions import FrozenInstanceError
 import msgspec
-import numpy as np
 import pytest
 
 from bystro.ancestry.ancestry_types import (
-    AncestryResponse,
-    AncestryResult,
-    AncestrySubmission,
+    AncestryResults,
+    AncestryScoresOneSample,
     AncestryTopHit,
-    AttrValidationError,
     PopulationVector,
     ProbabilityInterval,
     SuperpopVector,
@@ -27,34 +24,6 @@ from bystro.ancestry.train import POPS, SUPERPOPS
 # But the former method of checking raises E71 errors, which are
 # exempted from the linter on a file-wide basis above.
 
-
-def test_AncestrySubmission_accepts_valid_vcf_paths():
-    AncestrySubmission("foo.vcf")
-    AncestrySubmission("foo.vcf.gz")
-    AncestrySubmission("foo.vcf.lz4")
-    AncestrySubmission("foo.vcf.zstd")
-    AncestrySubmission("foo.vcf.bzip2")
-
-
-def test_AncestrySubmission_rejects_bad_vcf_paths():
-    with pytest.raises(AttrValidationError):
-        AncestrySubmission(3)  # type: ignore
-    with pytest.raises(AttrValidationError):
-        AncestrySubmission("foo.txt")
-    with pytest.raises(AttrValidationError):
-        AncestrySubmission("foo.gz")
-    with pytest.raises(AttrValidationError):
-        AncestrySubmission("foo.vcf.docx")
-    with pytest.raises(AttrValidationError):
-        AncestrySubmission("foo.txt.gz")
-
-
-def test_AncestrySubmission_is_frozen():
-    ancestry_submission = AncestrySubmission("foo.vcf")
-    with pytest.raises(FrozenInstanceError):
-        ancestry_submission.vcf_path = "bar.vcf"  # type: ignore
-
-
 prob_int = ProbabilityInterval(lower_bound=0.0, upper_bound=1.0)
 
 
@@ -65,25 +34,22 @@ def test_ProbabilityInterval_accepts_valid_bounds() -> None:
     assert type(prob_int.upper_bound) is float
 
 
-def test_ProbabilityInterval_rejects_bad_bounds() -> None:
-    with pytest.raises(AttrValidationError):
+def test_ProbabilityInterval_rejects_invalid_lower_bound() -> None:
+    with pytest.raises(TypeError, match="lower_bound must be >= 0.0"):
+        ProbabilityInterval(lower_bound=-0.1, upper_bound=0.9)
+
+
+def test_ProbabilityInterval_rejects_invalid_upper_bound() -> None:
+    with pytest.raises(TypeError, match="upper_bound must be <= 1.0"):
         ProbabilityInterval(lower_bound=0.1, upper_bound=1.1)
 
-    with pytest.raises(AttrValidationError):
-        ProbabilityInterval(lower_bound=-2, upper_bound=1.1)
 
-    err_msg = (
-        "Lower bound must be less than or equal to upper bound.  "
-        "Got: lower_bound=1.0, upper_bound=0.0 instead."
-    )
-    with pytest.raises(
-        AttrValidationError,
-        match=err_msg,
-    ):
-        ProbabilityInterval(lower_bound=1, upper_bound=0)
+def test_ProbabilityInterval_rejects_ints() -> None:
+    with pytest.raises(TypeError, match="lower_bound must be a float, not <class 'int'>"):
+        ProbabilityInterval(lower_bound=int(0), upper_bound=1.0)
 
-    with pytest.raises(FrozenInstanceError):
-        prob_int.lower_bound = 0.5  # type: ignore [misc]
+    with pytest.raises(TypeError, match="upper_bound must be a float, not <class 'int'>"):
+        ProbabilityInterval(lower_bound=0.0, upper_bound=int(1))
 
 
 pop_kwargs = {pop: prob_int for pop in POPS}
@@ -98,21 +64,15 @@ def test_PopulationVector_accepts_valid_args() -> None:
 def test_PopulationVector_rejects_missing_key() -> None:
     pop_kwargs_with_missing_key = pop_kwargs.copy()
     del pop_kwargs_with_missing_key["ACB"]
-    with pytest.raises(AttrValidationError):
+    with pytest.raises(TypeError, match="Missing required argument 'ACB'"):
         PopulationVector(**pop_kwargs_with_missing_key)
 
 
 def test_PopulationVector_rejects_extra_key() -> None:
     pop_kwargs_with_extra_key = pop_kwargs.copy()
     pop_kwargs_with_extra_key["FOO"] = prob_int
-    with pytest.raises(AttrValidationError):
+    with pytest.raises(TypeError, match="Unexpected keyword argument 'FOO'"):
         PopulationVector(**pop_kwargs_with_extra_key)
-
-
-def test_PopulationVector_is_frozen() -> None:
-    population_vector = PopulationVector(**pop_kwargs)
-    with pytest.raises(FrozenInstanceError):
-        population_vector.ACB = prob_int  # type: ignore [misc]
 
 
 def test_SuperpopVector_accepts_valid_args() -> None:
@@ -127,8 +87,8 @@ def test_SuperpopVector_accepts_valid_args() -> None:
 
 
 def test_SuperpopVector_rejects_missing_key() -> None:
-    with pytest.raises(AttrValidationError):
-        SuperpopVector(  # type: ignore [call-arg]
+    with pytest.raises(TypeError):
+        SuperpopVector(  # type: ignore
             AFR=prob_int,
             AMR=prob_int,
             EAS=prob_int,
@@ -137,8 +97,8 @@ def test_SuperpopVector_rejects_missing_key() -> None:
 
 
 def test_SuperpopVector_extra_key() -> None:
-    with pytest.raises(AttrValidationError):
-        SuperpopVector(  # type: ignore [call-arg]
+    with pytest.raises(TypeError):
+        SuperpopVector(  # type: ignore
             AFR=prob_int,
             AMR=prob_int,
             EAS=prob_int,
@@ -148,21 +108,8 @@ def test_SuperpopVector_extra_key() -> None:
         )
 
 
-def test_SuperpopVector_is_frozen() -> None:
-    superpop_vector = SuperpopVector(
-        AFR=prob_int,
-        AMR=prob_int,
-        EAS=prob_int,
-        EUR=prob_int,
-        SAS=prob_int,
-    )
-
-    with pytest.raises(FrozenInstanceError):
-        superpop_vector.AFR = prob_int  # type: ignore [misc]
-
-
-def test_AncestryResult_accepts_valid_args() -> None:
-    ancestry_result = AncestryResult(
+def test_AncestryScoresOneSample_accepts_valid_args() -> None:
+    ancestry_result = AncestryScoresOneSample(
         sample_id="my_sample_id",
         top_hit=AncestryTopHit(probability=0.6, populations=["SAS"]),
         populations=PopulationVector(**pop_kwargs),
@@ -172,9 +119,9 @@ def test_AncestryResult_accepts_valid_args() -> None:
     assert type(ancestry_result.missingness) is float
 
 
-def test_AncestryResult_rejects_invalid_missingness() -> None:
-    with pytest.raises(AttrValidationError):
-        AncestryResult(
+def test_AncestryScoresOneSample_rejects_invalid_missingness() -> None:
+    with pytest.raises(TypeError, match="missingness must be between 0.0 and 1.0"):
+        AncestryScoresOneSample(
             sample_id="my_sample_id",
             top_hit=AncestryTopHit(probability=0.6, populations=["SAS"]),
             populations=PopulationVector(**pop_kwargs),
@@ -182,67 +129,34 @@ def test_AncestryResult_rejects_invalid_missingness() -> None:
             missingness=1.1,
         )
 
-
-def test_AncestryResponse_rejects_bad_tophit_type() -> None:
-    with pytest.raises(AttrValidationError):
-        AncestryResult(
-            sample_id="foo",
-            top_hit=(0.5),  # type: ignore
+    with pytest.raises(TypeError, match="missingness must be a float, not <class 'int'>"):
+        AncestryScoresOneSample(
+            sample_id="my_sample_id",
+            top_hit=AncestryTopHit(probability=0.6, populations=["SAS"]),
             populations=PopulationVector(**pop_kwargs),
             superpops=SuperpopVector(**superpop_kwargs),
-            missingness=0.5,
-        )
-
-    with pytest.raises(AttrValidationError):
-        AncestryResult(
-            sample_id="foo",
-            top_hit=AncestryTopHit(probability=0.5),  # type: ignore
-            populations=PopulationVector(**pop_kwargs),
-            superpops=SuperpopVector(**superpop_kwargs),
-            missingness=0.5,
-        )
-
-    with pytest.raises(AttrValidationError):
-        AncestryResult(
-            sample_id="foo",
-            top_hit=AncestryTopHit(probability=np.float64(0.5), populations=["SAS"]),  # type: ignore
-            populations=PopulationVector(**pop_kwargs),
-            superpops=SuperpopVector(**superpop_kwargs),
-            missingness=0.5,
+            missingness=int(1),
         )
 
 
-def test_AncestryResult_is_frozen() -> None:
-    ancestry_result = AncestryResult(
-        sample_id="my_sample_id",
-        top_hit=AncestryTopHit(probability=0.6, populations=["SAS"]),
-        populations=PopulationVector(**pop_kwargs),
-        superpops=SuperpopVector(**superpop_kwargs),
-        missingness=0.1,
-    )
-    with pytest.raises(FrozenInstanceError):
-        ancestry_result.missingness = 0.2  # type: ignore [misc]
-
-
-def test_AncestryResponse_accepts_valid_args() -> None:
-    ancestry_response = AncestryResponse(
-        vcf_path="myfile.vcf",
+def test_AncestryResults_accepts_valid_args() -> None:
+    ancestry_response = AncestryResults(
         results=[
-            AncestryResult(
+            AncestryScoresOneSample(
                 sample_id="foo",
                 top_hit=AncestryTopHit(probability=0.6, populations=["EAS"]),
                 populations=PopulationVector(**pop_kwargs),
                 superpops=SuperpopVector(**superpop_kwargs),
                 missingness=0.5,
             ),
-            AncestryResult(
+            AncestryScoresOneSample(
                 sample_id="bar",
                 top_hit=AncestryTopHit(probability=0.7, populations=["EUR"]),
                 populations=PopulationVector(**pop_kwargs),
                 superpops=SuperpopVector(**superpop_kwargs),
                 missingness=0.5,
             ),
-            AncestryResult(
+            AncestryScoresOneSample(
                 sample_id="baz",
                 top_hit=AncestryTopHit(probability=0.5, populations=["AFR", "AMR"]),
                 populations=PopulationVector(**pop_kwargs),
@@ -253,15 +167,16 @@ def test_AncestryResponse_accepts_valid_args() -> None:
         pcs={"SampleID1": [0.1, 0.2, 0.3], "SampleID2": [0.4, 0.5, 0.6], "SampleID3": [0.7, 0.8, 0.9]},
     )
     ancestry_response_json = msgspec.json.encode(ancestry_response)
-    msgspec.json.decode(ancestry_response_json, type=AncestryResponse)
+    msgspec.json.decode(ancestry_response_json, type=AncestryResults)
 
 
-def test_AncestryResponse_rejects_invalid_pcs() -> None:
-    with pytest.raises(msgspec.ValidationError, match=r"Expected `object`, got `array`"):
-        ancestry_response = AncestryResponse(
-            vcf_path="myfile.vcf",
+def test_AncestryResults_rejects_invalid_pcs() -> None:
+    with pytest.raises(
+        msgspec.ValidationError, match=re.escape("Expected `object`, got `array` - at `$.pcs`")
+    ):
+        ancestry_response = AncestryResults(
             results=[
-                AncestryResult(
+                AncestryScoresOneSample(
                     sample_id="foo",
                     top_hit=AncestryTopHit(probability=0.6, populations=["EAS"]),
                     populations=PopulationVector(**pop_kwargs),
@@ -272,73 +187,4 @@ def test_AncestryResponse_rejects_invalid_pcs() -> None:
             pcs=({"SampleID1": [0.1]}, {"SampleID2": [0.4]}, {"SampleID3": [0.7]}),  # type: ignore
         )
         ancestry_response_json = msgspec.json.encode(ancestry_response)
-        msgspec.json.decode(ancestry_response_json, type=AncestryResponse)
-
-
-def test_AncestryResponse_rejects_bad_vcf_path() -> None:
-    with pytest.raises(AttrValidationError):
-        AncestryResponse(
-            vcf_path="foo.txt",
-            results=[],
-            pcs={
-                "SampleID1": [0.1, 0.2, 0.3],
-                "SampleID2": [0.4, 0.5, 0.6],
-                "SampleID3": [0.7, 0.8, 0.9],
-            },
-        )
-
-
-def test_AncestryResponse_rejects_bad_results_type() -> None:
-    with pytest.raises(AttrValidationError):
-        AncestryResponse(
-            vcf_path="myfile.vcf",
-            results=[3, 4, 5],  # type: ignore [list-item]
-            pcs={
-                "SampleID1": [0.1, 0.2, 0.3],
-                "SampleID2": [0.4, 0.5, 0.6],
-                "SampleID3": [0.7, 0.8, 0.9],
-            },
-        )
-
-
-def test_AncestryResponse_is_frozen() -> None:
-    ancestry_response = AncestryResponse(vcf_path="foo.vcf", results=[], pcs={})
-    with pytest.raises(FrozenInstanceError):
-        ancestry_response.vcf_path = "bar.vcf"  # type: ignore [misc]
-
-
-def test_AncestryResponse_rejects_duplicate_sample_ids() -> None:
-    with pytest.raises(
-        AttrValidationError, match=r"Expected unique sample_ids but found duplicated samples {'foo'}"
-    ):
-        AncestryResponse(
-            vcf_path="myfile.vcf",
-            results=[
-                AncestryResult(
-                    sample_id="foo",
-                    top_hit=AncestryTopHit(probability=0.6, populations=["EAS"]),
-                    populations=PopulationVector(**pop_kwargs),
-                    superpops=SuperpopVector(**superpop_kwargs),
-                    missingness=0.5,
-                ),
-                AncestryResult(
-                    sample_id="foo",
-                    top_hit=AncestryTopHit(probability=0.6, populations=["EAS"]),
-                    populations=PopulationVector(**pop_kwargs),
-                    superpops=SuperpopVector(**superpop_kwargs),
-                    missingness=0.5,
-                ),
-                AncestryResult(
-                    sample_id="bar",
-                    top_hit=AncestryTopHit(probability=0.7, populations=["EUR"]),
-                    populations=PopulationVector(**pop_kwargs),
-                    superpops=SuperpopVector(**superpop_kwargs),
-                    missingness=0.5,
-                ),
-            ],
-            pcs={
-                "SampleID1": [0.1, 0.2, 0.3],
-                "SampleID2": [0.4, 0.5, 0.6],
-                "SampleID3": [0.7, 0.8, 0.9],
-            },
-        )
+        msgspec.json.decode(ancestry_response_json, type=AncestryResults)
--- python/python/bystro/ancestry/tests/test_listener.py
+++ python/python/bystro/ancestry/tests/test_listener.py
@@ -1,22 +1,18 @@
 """Test ancestry listener."""
 
-from pathlib import Path
 from unittest.mock import patch
 
-import pytest
-
-from bystro.ancestry.ancestry_types import AncestrySubmission
 from bystro.ancestry.listener import (
     AncestryJobData,
-    completed_msg_fn,
     handler_fn_factory,
-    submit_msg_fn,
 )
-from bystro.ancestry.tests.test_inference import ANCESTRY_MODEL, FAKE_GENOTYPES
+from bystro.ancestry.tests.test_inference import (
+    ANCESTRY_MODEL,
+    FAKE_GENOTYPES,
+    FAKE_GENOTYPES_DOSAGE_MATRIX,
+)
 from bystro.beanstalkd.messages import ProgressMessage
 from bystro.beanstalkd.worker import ProgressPublisher
-
-FAKE_VCF_DIR = Path("my_fake_vcf_dir")
 
 
 handler_fn = handler_fn_factory(ANCESTRY_MODEL)
@@ -27,74 +23,14 @@ def test_handler_fn_happy_path():
     publisher = ProgressPublisher(
         host="127.0.0.1", port=1234, queue="my_queue", message=progress_message
     )
-    ancestry_submission = AncestrySubmission("foo.vcf")
-    ancestry_job_data = AncestryJobData(
-        submissionID="my_submission_id2", ancestry_submission=ancestry_submission
-    )
-    with patch("bystro.ancestry.listener._load_vcf", return_value=FAKE_GENOTYPES) as _mock:
+    ancestry_job_data = AncestryJobData(submissionID="my_submission_id2", dosage_matrix_path="some_path")
+    with patch("bystro.ancestry.listener._load_vcf", return_value=FAKE_GENOTYPES_DOSAGE_MATRIX) as _mock:
         ancestry_response = handler_fn(publisher, ancestry_job_data)
-    assert ancestry_submission.vcf_path == ancestry_response.vcf_path
 
+    # Demonstrate that all expected sample_ids are accounted for
+    samples_seen = set()
+    expected_samples = set(FAKE_GENOTYPES.columns)
+    for result in ancestry_response.results:
+        samples_seen.add(result.sample_id)
 
-def test_submit_msg_fn_happy_path():
-    ancestry_submission = AncestrySubmission("foo.vcf")
-    ancestry_job_data = AncestryJobData(
-        submissionID="my_submission_id", ancestry_submission=ancestry_submission
-    )
-    submitted_job_message = submit_msg_fn(ancestry_job_data)
-    assert submitted_job_message.submissionID == ancestry_job_data.submissionID
-
-
-def test_completed_msg_fn_happy_path():
-    progress_message = ProgressMessage(submissionID="my_submission_id")
-    publisher = ProgressPublisher(
-        host="127.0.0.1", port=1234, queue="my_queue", message=progress_message
-    )
-
-    ancestry_submission = AncestrySubmission("foo.vcf")
-    ancestry_job_data = AncestryJobData(
-        submissionID="my_submission_id", ancestry_submission=ancestry_submission
-    )
-
-    with patch("bystro.ancestry.listener._load_vcf", return_value=FAKE_GENOTYPES) as _mock:
-        ancestry_response = handler_fn(publisher, ancestry_job_data)
-    ancestry_job_complete_message = completed_msg_fn(ancestry_job_data, ancestry_response)
-
-    assert ancestry_job_complete_message.submissionID == ancestry_job_data.submissionID
-    assert ancestry_job_complete_message.results == ancestry_response
-
-
-def test_completed_msg_fn_rejects_nonmatching_vcf_paths():
-    progress_message = ProgressMessage(submissionID="my_submission_id")
-    publisher = ProgressPublisher(
-        host="127.0.0.1", port=1234, queue="my_queue", message=progress_message
-    )
-
-    ancestry_submission = AncestrySubmission("foo.vcf")
-    ancestry_job_data = AncestryJobData(
-        submissionID="my_submission_id", ancestry_submission=ancestry_submission
-    )
-
-    with patch("bystro.ancestry.listener._load_vcf", return_value=FAKE_GENOTYPES) as _mock:
-        _correct_but_unused_ancestry_response = handler_fn(publisher, ancestry_job_data)
-
-    progress_message = ProgressMessage(submissionID="my_submission_id")
-    publisher = ProgressPublisher(
-        host="127.0.0.1", port=1234, queue="my_queue", message=progress_message
-    )
-
-    # now instantiate another ancestry response with the wrong vcf...
-    wrong_ancestry_submission = AncestrySubmission("bar.vcf")
-    wrong_ancestry_job_data = AncestryJobData(
-        submissionID="my_submission_id", ancestry_submission=wrong_ancestry_submission
-    )
-
-    with patch("bystro.ancestry.listener._load_vcf", return_value=FAKE_GENOTYPES) as _:
-        wrong_ancestry_response = handler_fn(publisher, wrong_ancestry_job_data)
-    # end instantiating another ancestry response with the wrong vcf...
-
-    with pytest.raises(
-        ValueError,
-        match=r"Ancestry submission filename .*\.vcf doesn't match response filename .*\.vcf",
-    ):
-        _ancestry_job_complete_message = completed_msg_fn(ancestry_job_data, wrong_ancestry_response)
+    assert samples_seen == expected_samples
